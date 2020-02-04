f82aed2dc201115c5ac147cd2f9cbff3ecdd28f1
Eric DeJarnett <eric.dejarnett@gmail.com>
2020-02-04 12:28:40 -0700

    Added network action class

---
+++ POMDPy/examples/thesis/network-action.py
@@ -0,0 +1,11 @@
+from pomdpy.discrete_pomdp import DiscreteAction
+import sys
+from builtins import object
+
+class ActionType(object):
+	# enumerate actions
+	PLACEHOLDER = -1
+
+class NetworkAction(DiscreteAction):
+	# child class for network action
+	pass
