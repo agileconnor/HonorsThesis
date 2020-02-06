be10ecb7c4434d7c43d817c7f52b1127dde20178
agileconnor <connor.belanger@gmail.com>
2020-02-06 09:42:58 -0700

    Finished network processing

--- numpy_convert.py
+++ numpy_convert.py
@@ -10,16 +10,17 @@ def convertTransitions():
     t_list = t_list[1:]
     actions = []
     for row in t_list:
-        state0 = row[1:4]
-        state1 = row[4:7]
+        state0 = list(map(float, row[1:4]))
+        state1 = list(map(float, row[4:7]))
         actions.append([state0, state1])
 
-    print(actions)
+    #print(actions)
     num_actions = len(actions)
 
     transitions = numpy.array(actions)
 
-    print(transitions)
+    #print(transitions)
+    t_file.close()
 
     return transitions
 
@@ -32,9 +33,37 @@ def convertRewards():
 
 def processNetwork(netFile):
     n_file = open(netFile, 'r')
+    n_list = list(csv.reader(n_file))
+
+    n_list = n_list[1:]
+    computers = []
+    for row in n_list:
+        computer = list(map(int, row[1:]))
+        computers.append(computer)
+
+    return computers
 
 def generateTables(netFile):
     transitions = convertTransitions()
     rewards = convertRewards()
+    computers = processNetwork(netFile)
 
+    #print(computers)
+
+    arrays = []
+    for computer in computers:
+        newTran = numpy.copy(transitions)
+        for act in range(0, len(computer)):
+            #print(act)
+            newTran[act] = newTran[act] * computer[act]
+        arrays.append(newTran)
+
+    newTransitions = numpy.asarray(arrays)
+    #print(newTransitions)
+    newTransitions = numpy.mean(newTransitions, axis = 0)
+
+    print("Averaged Transition Table Generated")
+    print(newTransitions)
+
+    return [newTransitions, rewards]
 
