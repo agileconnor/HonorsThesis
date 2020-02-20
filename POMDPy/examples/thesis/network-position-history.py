649b94b07d6860ac90e60ada91d81fc12ab8306a
Eric DeJarnett <eric.dejarnett@gmail.com>
2020-02-20 11:45:34 -0700

    Added network position history tracker: manages overall process state

---
+++ POMDPy/examples/thesis/network-position-history.py
@@ -0,0 +1,50 @@
+from builtins import object
+import numpy as np
+from pomdpy.pomdp import HistoricalData
+from network_action import ActionType
+
+class NodeData(object):
+	def __init__(self):
+		self.check_count = 0
+		self.vuln = -1
+		
+	def to_string(self):
+		return 'Check count: '+str(self.check_count)+' Vulnerability: '+str(self.vuln)
+
+
+class HistoricalNetworkData(HistoricalData):
+	def __init__(self, model, network_position, all_node_data, solver):
+		self.model = model
+		self.solver = solver
+		self.network_position = network_position
+		self.all_node_data = all_node_data
+
+	def copy_node_data(other_data):
+		new_node_data = []
+		[new_node_data.append(NodeData()) for _ in other_data]
+		for i, j in zip(other_data, new_node_data):
+            j.check_count = i.check_count
+            j.vuln = i.vuln
+        return new_node_data
+
+	def copy(self):
+		return self.shallow_copy()
+
+	def deep_copy(self)
+		return HistoricalNetworkData(self.model, self.network_position.copy(), self.all_node_data, self.solver)
+
+	def shallow_copy(self):
+		new_node_data = self.copy_node_data(self.all_node_data)
+		return HistoricalNetworkData(self.model, self.network_position.copy(), new_node_data, self.solver)
+
+	def update(self, other_belief):
+		self.all_rock_data = other_belief.data.all_rock_data
+
+	def create_child(self, node_action, rock_observation):
+		next_data = self.deep_copy()
+		next_position, is_legal = self.model.make_next_position(self.grid_position.copy(), node_action.bin_number)
+		next_data.grid_position = next_position
+
+		if node_action.bin_number is ActionType.SCAN:
+			node_no = self.model.get_cell_type(self.network_position)
+			# FINISH ME PLEASE #
