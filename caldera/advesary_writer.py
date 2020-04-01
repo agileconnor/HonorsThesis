7fbdca16b16a4a8e5114b63a5df5aeb3bbaa3d11
agileconnor <connor.belanger@gmail.com>
2020-03-31 22:56:24 -0700

    Small Update to adversary writer

--- caldera/advesary_writer.py
+++ caldera/advesary_writer.py
@@ -25,3 +25,23 @@ adv_file.write("    - "+uuid_dict["SMTP"]+"\n")
 
 adv_file.close()
 print("Adversary Created")
+
+tgt_list = ['10.0.2.5', '10.0.2.6','10.0.2.7']
+
+src_id = str(uuid.uuid4())
+src_file = open("sources/"+src_id+".yml","w")
+src_file.write("---\n\n")
+src_file.write("id: "+src_id+"\n")
+src_file.write("name: AI Filters\n")
+src_file.write("facts: {}\n")
+src_file.write("rules:\n")
+src_file.write("  - action: DENY\n")
+src_file.write("    fact: my.host.ip\n")
+src_file.write("    match: .*\n")
+
+for tgt in tgt_list:
+    src_file.write("  - action: ALLOW\n")
+    src_file.write("    fact: my.host.ip\n")
+    src_file.write("    match: "+tgt+"\n")
+
+print("Rules for "+str(len(tgt_list))+" targets created")
